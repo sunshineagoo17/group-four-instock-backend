0811cac88acffb2cf067cbfb1dadd6ab3196026c
Camilo <user@CAmilos-MacBook-Pro.local>
2024-05-12 21:02:09 -0700

    line 16 to 15 deleted

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const knex = require('knex')(require('../knexfile'));
 
-// Endpoint to get all warehouses - added here for testing purposes
+// Endpoint to get all warehouses - 
 router.get('/', async (req, res) => {
     try {
         const warehouses = await knex('warehouses').select('*');
@@ -12,17 +12,5 @@ router.get('/', async (req, res) => {
         res.status(500).send('Error fetching warehouses');
     }
 });
-
-// GET endpoint to fetch all warehouses brotha
-router.get('/warehouses', async (req, res) => {
-    try {
-        const warehouses = await knex('warehouses').select('*');
-        res.status(200).json(warehouses);
-    } catch (error) {
-        console.error('Error fetching warehouses:', error);
-        res.status(500).send('Error fetching warehouses');
-    }
-});
-
 
 module.exports = router;

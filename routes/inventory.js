4bc042c9f9e01bbea9f63cfc04562e280293fb29
Karlan Wilson <karlan@Karlans-MacBook-Pro-572.local>
2024-05-13 22:24:06 -0400

    Reframed index.js app.use statements to include routing for /api/inventories and /api/warehouses instead of /inventories and /warehouses. Used join statement to join both warehouses and inventories tables to allow for warehouse_name to be a key for the inventory objects. Remove console.log from catch statement.

--- routes/inventory.js
+++ routes/inventory.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const knex = require('knex')(require('../knexfile'));
 
-// Endpoint to get all inventory - added here for testing purposes
 router.get('/', async (req, res) => {
     try {
-        const inventories = await knex('inventories').select('*');
-        res.json(inventories);
+        const inventoriesWithWarehouseName = await knex('inventories')
+        .join('warehouses','warehouses.id','warehouse_id')
+        // .where({warehouse_id: warehouses.id})
+        .select('inventories.id','warehouses.warehouse_name','inventories.item_name','inventories.description','inventories.category','inventories.status','inventories.quantity');
+        res.json(inventoriesWithWarehouseName);
     } catch (error) {
-        console.error('Error fetching inventories:', error);
         res.status(500).send('Error fetching inventories');
     }
 });

e2c0f81e2f315433c09db808a09634132edb1268
Karlan Wilson <karlan@Karlans-MacBook-Pro-572.local>
2024-05-14 20:57:02 -0400

    Used req.params.id in where clause to locate id used in url. Added catch error with a status of 400 and error messaging. Used join to ensure we are able to select the warehouse name from warehouses table. Created if statement in the case that the inventory ID was not found. Used if statement with array.length === 0 as SQL will return an empty array if the req.param.id does not match.

--- routes/inventory.js
+++ routes/inventory.js
@@ -19,4 +19,26 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+router.get('/:id', async (req,res)=>{
+
+    try{
+        const selectedInventoryItem = await knex('inventories')
+        .join('warehouses','warehouses.id','warehouse_id')
+        .where({'inventories.id':req.params.id})
+        .select('inventories.id',
+        'warehouses.warehouse_name',
+        'inventories.item_name',
+        'inventories.description',
+        'inventories.category',
+        'inventories.status',
+        'inventories.quantity');
+        if(selectedInventoryItem.length === 0){
+            return res.status(404).send('Error: ID was not found');
+        }
+        res.status(200).json(selectedInventoryItem);
+    }catch(error){
+        res.status(500).send(`Error fetching single inventory item:${error}`);
+    }
+});
 module.exports = router;

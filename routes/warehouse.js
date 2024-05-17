25afc4eefa2e7de6a7d9b1f39b2b7a73d9b21b76
Karlan Wilson <karlan@Karlans-MacBook-Pro-572.local>
2024-05-16 20:17:52 -0400

    Created put router expression. Added validation function to router expression. Created if statement to check if warehouse id used in params exists. Created if statement to check if there were any errors in validation. Used else statement to create a response in json of the edited object. Install express-validator.

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -129,4 +129,34 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
+//Endpoint to edit a warehouse 
+
+router.put('/:id',validateWarehouse, async (req,res)=>{
+    const { id } = req.params;
+    const editRequest = validationResult(req)
+    try{
+        const warehouseInfo = await knex('warehouses')
+        .where('id',id)
+        .first();
+        if(!warehouseInfo){
+            res.status(404).send('Warehouse not found')
+        }
+        if(!editRequest.isEmpty()){
+            res.status(400).send('Data is incomplete or invalid')
+        }else{
+            const updatedInfo = await knex('warehouses')
+            .select('warehouse_name','address','city','country','contact_name','contact_position','contact_phone','contact_email')
+            .where('id',id)
+            .update(req.body)
+            .first()
+
+            return res.status(200).json(updatedInfo);
+        }
+
+   } catch(error){
+    res.status(500).send(`Error editing warehouse ${error.message}`);
+   }
+})
+
+
 module.exports = router;
